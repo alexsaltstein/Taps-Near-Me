fe3f4e5bc151842e79dbfe38f8cfc26efe2178a7
Alex Saltstein <asaltste@gmail.com>
2021-09-20 20:44:44 -0400

    added colors

--- taps-react-native/src/styles/COLORS.js
+++ taps-react-native/src/styles/COLORS.js
@@ -1,6 +1,11 @@
 const COLORS = {
   white: '#ffffff',
-  black: '#000000'
+  black: '#000000',
+  transparent: '#00000000',
+  green: '#18B126',
+  purple: '#A36BD3',
+  gray: '#707070',
+  red: '#E24242',
 }
 
 export { COLORS }
