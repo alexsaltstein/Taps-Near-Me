87809174741b91942d2e23cf242d121e760f6fc0
Alex Saltstein <asaltste@gmail.com>
2021-10-05 20:31:29 -0400

    added route to get beer by id

--- taps-api/routes/api/beers/beers.js
+++ taps-api/routes/api/beers/beers.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const dataHelpers = require('../../../data');
+const beersData = dataHelpers.beers;
 
 const router = express.Router();
 
@@ -6,6 +8,19 @@ router.get('/all', async (req, res) => {
   try {
     return res.status(200).json({
       message: 'Found Valid beers',
+    });
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const beer = await beersData.getBeerById(id);
+    return res.status(200).json({
+      message: 'Found Valid beer',
+      beer
     });
   } catch (e) {
     return res.status(500).json({ message: e.message });
