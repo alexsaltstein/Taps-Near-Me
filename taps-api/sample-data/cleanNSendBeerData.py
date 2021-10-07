9027ed41e17d3386e0343716da48f5617973eccf
ztylergeorge <ztylergeorge@gmail.com>
2021-10-06 20:10:57 -0400

    Creation of clean beer data file

--- taps-api/sample-data/cleanNSendBeerData.py
+++ taps-api/sample-data/cleanNSendBeerData.py
@@ -1,35 +1,47 @@
-from typing import List, Dict
+from typing import List, Dict, IO
 import json
+import os
 
-#props that are unwanted
-unwanted: List[str] = ['comment', 'rating_score','created_at','checkin_url','purchase_venue',
-                        'checkin_id','photo_url','global_weighted_rating_score','tagged_friends',
-                        'total_toasts','total_comments']
+def clean_beer_data(beer_data_file, cleaned_data_file):
 
-#Note: rating_score is user rating -- not global rating 
+    """ Clean the craft beer data in the folder and create cleaned file """
 
-#initiate new python file name 
-new_file: str = ""
+    #props that are unwanted
+    unwanted: List[str] = ['comment', 'rating_score','created_at','checkin_url','purchase_venue',
+                            'checkin_id','photo_url','global_weighted_rating_score','tagged_friends',
+                            'total_toasts','total_comments']
 
-#input file directory
+    #Note: rating_score is user rating -- not global rating 
 
-#load JSON data to dict
+    #change file directory
 
-fp = open("untappd-data-json.json", encoding="utf8")
-json_beer_data: Dict[str,str] = json.load(fp) 
+    os.chdir("C:/Users/ztyle/Documents/SSW 695/Taps-Near-Me/taps-api/sample-data")
 
-#iterate through JSON and only include wanted columns
+    #load JSON data to dict
 
-for beer in json_beer_data:
+    fp: IO = open(beer_data_file, encoding="utf8")
+    json_beer_data: List[Dict] = json.load(fp) 
 
-    for key in dict(beer).keys():
+    #iterate through JSON and only include wanted columns
 
-        if key in unwanted:
+    for beer in json_beer_data:
 
-            beer.pop(key)
+        for key in dict(beer).keys():
 
-#remove beers that are from Untappd at home or venue is None 
+            if key in unwanted:
 
-json_beer_data = [beer for beer in json_beer_data if beer['venue_name'] not in ['Untappd at Home', None, '']]
+                beer.pop(key)
 
-#write to new file
+    #remove beers that are from Untappd at home or venue is None 
+
+    json_beer_data: List[Dict] = [beer for beer in json_beer_data if beer['venue_name'] not in ['Untappd at Home', None, '']]
+
+    #load (dump) data into new json file
+
+    new_file = open(cleaned_data_file, "w")
+
+    json.dump(json_beer_data, new_file, skipkeys=False, ensure_ascii=True, check_circular=True, allow_nan=True, cls=None, indent=6, separators=None, default=None, sort_keys=False)
+
+    new_file.close()
+
+clean_beer_data("untappd-data-json.json", "cleaned-untappd-beer-data-json.json")
