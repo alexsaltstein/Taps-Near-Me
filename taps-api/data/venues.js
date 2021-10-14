653fc45b7a6dfacbe1a98f429c5ac318ff94c720
Alex Saltstein <asaltste@gmail.com>
2021-10-14 19:22:34 -0400

    modified query

--- taps-api/data/venues.js
+++ taps-api/data/venues.js
@@ -24,20 +24,17 @@ module.exports = {
     if (typeof city !== 'string' || city.trim() === '') throw new Error('city must be a non-empty string');
     if (typeof state !== 'string' || state.trim() === '') throw new Error('state must be a non-empty string');
     if (typeof country !== 'string' || country.trim() === '') throw new Error('country must be a non-empty string');
-    if (typeof lat !== 'string' || lat.trim() === '') throw new Error('lat must be a non-empty string');
-    if (typeof lng !== 'string' || lng.trim() === '') throw new Error('lng must be a non-empty string');
 
-    const venuesCollection = await venues();
-    let longitude = parseFloat(lng);
-    let latitude = parseFloat(lat);
+    const longitude = parseFloat(lng);
+    const latitude = parseFloat(lat);
     if (isNaN(longitude) || longitude > 180 || longitude < -180 ) throw 'lng must be a number between -180 and 180!';
     if (isNaN(latitude) || latitude > 90 || latitude < -90 ) throw 'lat must be a number between -90 and 90!';
     // schmema changed to be in geojson format
     const newVenue = {
+      _id: uuid.v4(),
       type: "Point",
       coordinates: [longitude, latitude],
       properties: {
-        _id: uuid.v4(),
         name,
         city,
         state,
@@ -45,6 +42,8 @@ module.exports = {
         beersAvailable: []
       }
     };
+
+    const venuesCollection = await venues();
 
     const insertedInfo = await venuesCollection.insertOne(newVenue);
     const newId = insertedInfo.insertedId;
