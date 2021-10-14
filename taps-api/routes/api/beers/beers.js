e5d20960d67e1cafd6f079b3948e0bb97db48f1b
Alex Saltstein <asaltste@gmail.com>
2021-10-14 19:10:15 -0400

    stash

--- taps-api/routes/api/beers/beers.js
+++ taps-api/routes/api/beers/beers.js
@@ -16,9 +16,35 @@ router.get('/all', async (req, res) => {
   }
 });
 
+router.get('/markers', async (req, res) => {
+  try {
+    return res.status(200).json({
+      message: 'Found Valid markers',
+      markers: [
+        {
+          title: 'beer store',
+          lat: 37,
+          lng: 122
+        },
+        { title: 'beer',
+        lat: 38,
+        lng: 125 },
+        { title: 'beer you',
+        lat: 32,
+        lng: 124 },
+        { title: 'beer me',
+        lat: 28,
+        lng: 122 },
+      ]
+    });
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
+  }
+});
+
 router.get('/:id', async (req, res) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const beer = await beersData.getBeerById(id);
     return res.status(200).json({
       message: 'Found Valid beer',
