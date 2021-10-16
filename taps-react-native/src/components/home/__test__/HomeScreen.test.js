a885cca9e0bd42a82ed003bb9a7629d07330b47c
ztylergeorge <ztylergeorge@gmail.com>
2021-10-15 21:26:08 -0400

    Start of FilterScreen and HomeScreen tests

---
+++ taps-react-native/src/components/home/__test__/HomeScreen.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import HomeScreen from '../HomeScreen';
+import ReactDOM from 'react-dom';
+
+test('renders correctly', () => {
+  const tree = renderer.create(<HomeScreen />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
