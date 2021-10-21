882c92cc22ec7a435a2a3ecc0c2ecea7a02c15b3
Connorsmith25 <71672667+Connorsmith25@users.noreply.github.com>
2021-10-21 17:25:18 -0400

    Update venues.js

--- taps-api/routes/api/venues/venues.js
+++ taps-api/routes/api/venues/venues.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const venuesRouter = require('./venues');
+const venuesData = dataHelpers.venues;
 
 const router = express.Router();
 
-router.use('/', venuesRouter);
-
+router.get('/filter', async (req, res) => {
+  try {
+    const location = [location[0], location[1]];
+    const filter = radius;
+    if (req.query.type){
+      filter.type = req.query.type;
+    }
+    const venues = await venuesData.getVenuesByRadius(location, radius);
+    return res.status(200).json({
+      message: 'Found Venues within radius',
+      venues    
+    });
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
+  }
+});
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const venue = await beersData.getVenueById(id);
+    return res.status(200).json({
+      message: 'Found Valid venue',
+      venue
+    });
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
+  }
+});
 module.exports = router;
