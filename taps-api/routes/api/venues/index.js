3af45ea2000471f4651f693b033103fa18ef5081
Alex Saltstein <asaltste@gmail.com>
2021-10-21 18:59:14 -0400

    added implementation

--- taps-api/routes/api/venues/index.js
+++ taps-api/routes/api/venues/index.js
@@ -1,20 +1,8 @@
 const express = require('express');
-const dataHelpers = require('../../../data');
-const beersData = dataHelpers.beers;
+const venuesRouter = require('./venues');
 
 const router = express.Router();
 
-router.get('/beers/:id', async (req, res) => {
-  try {
-    const {id} = req.params;
-    const venue = await beersData.getBeersByVenue(id);
-    return res.status(200).json({
-      message: 'Found Valid beers for venue',
-      venue
-    });
-  } catch (e) {
-    return res.status(500).json({ message: e.message });
-  }
-});
+router.use('/', venuesRouter);
 
 module.exports = router;
--- taps-api/routes/api/venues/venues.js
+++ taps-api/routes/api/venues/venues.js
@@ -1,8 +1,34 @@
 const express = require('express');
-const venuesRouter = require('./venues');
+const dataHelpers = require('../../../data');
+const beersData = dataHelpers.beers;
+const venuesData = dataHelpers.venues;
 
 const router = express.Router();
 
-router.use('/', venuesRouter);
+router.get('/beers/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const venue = await beersData.getBeersByVenue(id);
+    return res.status(200).json({
+      message: 'Found Valid beers for venue',
+      venue
+    });
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
+  }
+});
+
+router.get('/location', async (req, res) => {
+  try {
+    const {lat, lng, radius} = req.query;
+    const venues = await venuesData.getVenuesByRadius([lng, lat], radius);
+    return res.status(200).json({
+      message: 'Found venues within that location',
+      venues
+    });
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
+  }
+});
 
 module.exports = router;
