33bc97e018954e3177af1c5e99365d2db2930d53
ztylergeorge <ztylergeorge@gmail.com>
2021-10-28 23:59:57 -0400

    initial push to newHomeScreen

--- taps-react-native/src/components/home/HomeScreen.js
+++ taps-react-native/src/components/home/HomeScreen.js
@@ -3,6 +3,7 @@ import { Text, TouchableOpacity, View, StyleSheet} from 'react-native';
 import Logo from '../widgets/Logo';
 import filter from '../../../assets/navigation/adjust-alt.png';
 import settings from '../../../assets/navigation/settings.png';
+import map from '../../../assets/navigation/map.png'
 import NavIcons from '../widgets/NavIcons';
 import setStatusBarColor from '../utils/StatusBarColorFunctions';
 import { COLORS } from '../../styles/COLORS';
@@ -26,7 +27,8 @@ const HomeScreen = ({ navigation }) => {
         <View style={styles.buttonsContainer}>
           {
             [{ icon: filter, to: 'Filter' },
-            { icon: settings, to: 'Settings' }].map(i => (
+            { icon: settings, to: 'Settings' }, 
+            { icon: map, to: 'Map'}].map(i => (
               <NavIcons
                 key={i.to}
                 source={i.icon}
@@ -36,10 +38,19 @@ const HomeScreen = ({ navigation }) => {
           }
         </View>
       </View>
-      <Text>What are we looking to drink?</Text>
-      <TouchableOpacity onPress={()=>{navigation.navigate('Map')}}>
-        <Text>Map</Text>
-      </TouchableOpacity>
+      <Text>How to Use Taps Near Me</Text>
+      <Text>1. Click on the Filter Icon</Text>
+      <Text>2. Choose your desired search flter</Text>
+      <Text>ABV Range: How strong do you want your beer to be?</Text>
+      <Text>IBU Range: How bitter do you want your beer to be?</Text>
+      <Text>Style: What type do you want your beer to be?</Text>
+      <Text>Minimum Global Rating: How good do you want your beer to be?</Text>
+      <Text>Distance: How far do you want your beer to be?</Text>
+      <Text>3. Save your search flter</Text>
+      <Text>4. Click on the Map Icon</Text>
+      <Text>5. Find where the taps are near you!</Text>
+      <Text>No taps found? No problem! Just refine your search until taps are found.</Text>
+      <Text>There are currently over 150 beers available!</Text>
     </View>
   );
 };
@@ -47,7 +58,7 @@ const HomeScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: 'flex-start',
   },
   header: {
     flexDirection: 'row',
@@ -56,7 +67,7 @@ const styles = StyleSheet.create({
   },
   buttonsContainer: {
     flexDirection: 'row',
-    alignItems: 'center',
-  },
+    alignItems: 'center'
+  }
 })
 export default HomeScreen;
