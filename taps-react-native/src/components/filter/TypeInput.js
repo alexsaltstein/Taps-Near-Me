ded3d7c1bf0c8f8596e58632cfdfda13b35e45ab
Alex Saltstein <asaltste@gmail.com>
2021-12-01 17:31:16 -0500

    spacing:

--- taps-react-native/src/components/filter/TypeInput.js
+++ taps-react-native/src/components/filter/TypeInput.js
@@ -10,7 +10,7 @@ const TypeInput = ({ label, curr, setCurr }) => {
       <TextInput
         placeholder="Style - Substyle"
         placeholderTextColor={COLORS.lightgray}
-        style={{ width: '95%', alignSelf:'center', marginTop: 5, fontFamily: 'open-sans', fontSize: 15, height: 40, borderWidth: 2, borderColor: COLORS.white, borderRadius: 10, padding: 10, color: COLORS.white, }}
+        style={{ width: '95%', alignSelf:'center', marginTop: 5, fontFamily: 'open-sans', fontSize: 15, height: 50, borderWidth: 2, borderColor: COLORS.white, borderRadius: 10, padding: 10, color: COLORS.white, }}
         value={curr}
         onChangeText={(val) => setCurr(val)} />
     </View>
